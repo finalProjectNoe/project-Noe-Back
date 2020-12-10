e1900d838b2d5c7c0bee2994c79b54a8ac4cf67c
Miklsan <mborghmans@gmail.com>
2020-12-10 18:00:51 +0100

    some new tests

--- contracts/Noe.sol
+++ contracts/Noe.sol
@@ -4,24 +4,24 @@ pragma experimental ABIEncoderV2;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 /// @title Un contrat de passeport animaliers
 /// @author Théo, Streed, Nico, Mika
 /// @notice Ce contrat permet d'associer des animaux à des utilisateurs via des vétérinaires
 
-contract Noe is ERC721 {
-    
+contract Noe is ERC721, Ownable {
     using Counters for Counters.Counter; // Utilisation du contract Counter d'openzeppelin importait en ligne 6
     Counters.Counter private _tokenIds; // Utilisation de la fonction Counter pour associer un _tokenIds
 
     address payable private _superAdmin; // Adresse de la personne qui déploie
 
     /// @notice Name and symbol of the non fungible token, as defined in ERC721.
-    constructor(address payable superAdmin) public ERC721("Noe", "NOE") { // Constucteur
+    constructor(address payable superAdmin) public ERC721("Noe", "NOE") {
+        // Constucteur
+        transferOwnership(superAdmin);
         _superAdmin = superAdmin;
     }
-    
-    uint256 private animalsCount; // Variable de statut pour compter le nombre d'animaux
 
     enum Animals {dog, cat, ferret} // Enumération
 
@@ -33,13 +33,15 @@ contract Noe is ERC721 {
 
     event AnimalToken(address _address); // Event pour qu'un vétérinaire crée un animal
 
-    struct Member { // Structure membres
+    struct Member {
+        // Structure membres
         string name; // Nom du membre
         uint256 tel; // Numéro de téléphone du membre
         bool isMember; // False par défaut, true si la pérsonne est déjà enregistré
     }
 
-    struct Animal { // Structure animales
+    struct Animal {
+        // Structure animales
         string name; // Nom de l'animal
         string dateBirth; // Date de naissance de l'animal
         string sexe; // Sexe de l'animal
@@ -47,21 +49,24 @@ contract Noe is ERC721 {
         Animals animals; // Enumération de chien chat furret
     }
 
-    struct Veterinary { // Structure vétérinaire
+    struct Veterinary {
+        // Structure vétérinaire
         string name; // Nom du vétérinaire
         uint256 tel; // Numéro de téléphone du vétérinaire
         bool diploma; // False par defaut, le super admin approuve le vétérinaire une fois les diplomes valide
         bool isVeterinary; // False par defaut, il devient vétérinaire une fois que le super admin approuve
     }
 
+    uint256 public animalsCount; // Variable de statut pour compter le nombre d'animaux
+
     /// @dev Mapping de la struct Animal
     mapping(uint256 => Animal) private _animal;
 
     /// @dev Mapping de la struct Member
-    mapping(address => Member) private member;
+    mapping(address => Member) public member;
 
     /// @dev Mapping de la struct Veterinary
-    mapping(address => Veterinary) private veterinary;
+    mapping(address => Veterinary) public veterinary;
 
     // This modifer, vérifie si c'est le super admin
     modifier isSuperAdmin() {
@@ -74,9 +79,9 @@ contract Noe is ERC721 {
         require(member[msg.sender].isMember == true, "Vous n'étes pas membre");
         _;
     }
-    
+
     // This modifer, vérifie si le membre n'est pas déjà enregistré
-    modifier onlyNotMember() {
+    modifier onlyYetMember() {
         require(member[msg.sender].isMember == false, "Vous étes déjà membre");
         _;
     }
@@ -86,9 +91,9 @@ contract Noe is ERC721 {
         require(veterinary[msg.sender].isVeterinary == true, "Vous n'étes pas vétérinaire");
         _;
     }
-    
+
     // This modifer, vérifie si le vétérinaire n'est pas déjà enregistré
-    modifier onlyNotVeterinary() {
+    modifier yetOnlyVeterinary() {
         require(veterinary[msg.sender].isVeterinary == false, "Vous étes déjà vétérinaire");
         _;
     }
@@ -102,7 +107,7 @@ contract Noe is ERC721 {
     /// @dev Permet de créer un nouveau membre en vérifiant qu'il n'est pas déjà membre
     /// @param _name set le nom du membre dans la struct Member
     /// @param _tel set le nom du téléphone dans la struct Member
-    function createMember(string memory _name, uint256 _tel) public onlyNotMember() {
+    function createMember(string memory _name, uint256 _tel) public onlyYetMember() {
         member[msg.sender] = Member({name: _name, tel: _tel, isMember: true});
 
         emit MemberCreated(msg.sender); /// emit de l'event MemberCreated
@@ -111,7 +116,7 @@ contract Noe is ERC721 {
     /// @dev Permet de créer un compte vétérinaire sous réserve de validation du diplôme par le super admin et la fonction approveVeterinary
     /// @param _name set le nom du membre dans la struct vétérinaire
     /// @param _tel set le nom du téléphone dans la struct vétérinaire
-    function createVeterinary(string memory _name, uint256 _tel) public onlyNotVeterinary() {
+    function createVeterinary(string memory _name, uint256 _tel) public yetOnlyVeterinary() {
         veterinary[msg.sender] = Veterinary({name: _name, tel: _tel, diploma: false, isVeterinary: false});
 
         emit VeterinaryCreated(msg.sender);
@@ -119,13 +124,22 @@ contract Noe is ERC721 {
 
     /// @dev Permet de valider le compte vétérinaire après vérification du diplôme
     /// @param _addr passe l'adresse du vétérinaire à approuver
-    function approveVeterinary(address _addr) public isSuperAdmin {
+    function approveVeterinary(address _addr) public isSuperAdmin returns (bool) {
         veterinary[_addr].diploma = true; // Set à true le diplome dans la struct Veterinary
         veterinary[_addr].isVeterinary = true; // Set à true, il devient vétérinaire dans la struct Veterinary
 
         emit VeterinaryApprove(msg.sender); /// emit de l'event VeterinaryApprove
     }
 
+    /// @dev Permet de se connecter en tant que membre
+    /// @param _addr de connexion du membre
+    /// @param _name nom du membre
+    function connectionMember(address _addr, string memory _name) public onlyMember() {}
+
+    /// @dev Permet de se connecter en tant que vétérinaire
+    /// @param _addr de connexion du vétérinaire
+    /// @param _name nom du vétérinaire
+    function connectionVeterinary(address _addr, string memory _name) public onlyVeterinary() {}
 
     /// @dev Crée un animal et lui associe un token ERC721
     /// @param _member du membre à qui attibuer l'animal/token
@@ -143,18 +157,23 @@ contract Noe is ERC721 {
         bool _vaccin,
         Animals animals_
     ) public onlyVeterinary() returns (uint256) {
-        animalsCount++;
         _tokenIds.increment();
         uint256 newTokenId = _tokenIds.current();
         _mint(_member, newTokenId);
-        _animal[newTokenId] = Animal({name: _name, dateBirth: _dateBirth, sexe: _sexe, vaccin: _vaccin, animals: animals_});
+        _animal[newTokenId] = Animal({
+            name: _name,
+            dateBirth: _dateBirth,
+            sexe: _sexe,
+            vaccin: _vaccin,
+            animals: animals_
+        });
         emit AnimalToken(msg.sender); /// emit de l'event AnimalToken
         return newTokenId;
     }
 
     /// @dev Permet de retrouver un animal en fonction de son numéro de token
     /// @param tokenId retrouver un animal via son numéro de token
-    function getAnimalById(uint256 tokenId) public view animalIdCheck(tokenId) returns (Animal memory) {
+    function getAnimalById(uint256 tokenId) public view returns (Animal memory) {
         require(_exists(tokenId), "NOE: Animal query for no nexistent token");
         return _animal[tokenId];
     }
--- test/Noe_test.js
+++ test/Noe_test.js
@@ -5,18 +5,18 @@ const { expect } = require('chai');
 
 const Noe = contract.fromArtifact('Noe');
 
-/* const isSameAnimal = (_animal1, animal1) => {
+const isSameAnimal = (_animal1, animal1) => {
   return (
     _animal1 === animal1[0] &&
     new BN(_animal1[1]).eq(animal1[1]) &&
     new BN(_animal1[2]).eq(animal1[2]) &&
     new BN(_animal1[3]).eq(animal1[3])
   );
-}; */
+};
 
 describe('Noe', function () {
   this.timeout(0);
-  const [owner, dev, user1, veterinary1, vetApprouve1] = accounts;
+  const [owner, dev, user1, user2, veterinary1, vetApprouve1, superAdmin] = accounts;
   const NAME = 'Noe';
   const SYMBOL = 'NOE';
   // const animal1 = ['Murphy', '17/01/2020', 'Male', true, new BN(0)];
@@ -24,28 +24,60 @@ describe('Noe', function () {
   // const animal3 = ['Gospelle', '17/01/2020', 'Femelle', true, new BN(2)];
   // const animal4 = ['Patch', '17/01/2020', 'Male', true, new BN(1)];
   const userTest1 = ['Théo', new BN(0, 6, 6, 8, 3, 1, 2, 4, 8, 5), true];
-  const userVeterinary1 = ['Streed', new BN(0, 6, 6, 8, 3, 1, 2, 4, 8, 5), false, false]
+  const userVeterinary1 = ['Streed', new BN(0, 6, 6, 8, 3, 1, 2, 4, 8, 5), false, false];
   const vetApprouve = [true, true];
+  const animal1 = ['0x67b3dc27E5fc43f0C491eCcCEcb9588Df2042501', 'Murphy', '17/01/2020', 'Male', true, new BN(0)];
+  const animal2 = ['0x67b3dc27E5fc43f0C491eCcCEcb9588Df2042502', 'Pixel', '17/01/2020', 'Male', true, new BN(1)];
+  const animal3 = ['0x67b3dc27E5fc43f0C491eCcCEcb9588Df2042503', 'Gospelle', '17/01/2020', 'Femelle', true, new BN(2)];
+  const animal4 = ['0x67b3dc27E5fc43f0C491eCcCEcb9588Df2042504', 'Patch', '17/01/2020', 'Male', true, new BN(1)];
 
   beforeEach(async function () {
-    this.animal = await Noe.new(owner, { from: dev });
-    this.member = await Noe.new(user1, { from: user1 });
-    this.veterinary = await Noe.new(veterinary1, { from: veterinary1 });
-    this.approuve = await Noe.new(vetApprouve1, { from: owner });
+    this.noe = await Noe.new(owner, { from: dev });
   });
   it('A un nom', async function () {
-    expect(await this.animal.name()).to.equal(NAME);
+    expect(await this.noe.name()).to.equal(NAME);
   });
   it('A un symbol', async function () {
-    expect(await this.animal.symbol()).to.equal(SYMBOL);
+    expect(await this.noe.symbol()).to.equal(SYMBOL);
+  });
+  it('has owner', async function () {
+    expect(await this.noe.owner()).to.equal(owner);
   });
   it('Membre crée', async function () {
-    await this.member.createMember(user1, userTest1, { from: user1 });
+    await this.noe.createMember(user1, userTest1, { from: user1 });
   });
   it('Vétérinaire crée', async function () {
-    await this.veterinary.createVeterinary(veterinary1, userVeterinary1, { from: veterinary1 });
+    await this.noe.createVeterinary(userVeterinary1[0], userVeterinary1[1], { from: veterinary1 });
+    const vet1 = await this.noe.getVeterinary(userVeterinary1);
+    expect(vet1[0]).to.be.equal(userVeterinary1[0]);
+    expect(vet1[1]).to.be.equal(userVeterinary1[1]);
+    expect(vet1[2]).to.be.equal(userVeterinary1[2]);
+    expect(vet1[3]).to.be.equal(userVeterinary1[3]);
   });
   it('Vétérinaire approuvé', async function () {
-    await this.veterinary.approveVeterinary(vetApprouve, { from: owner });
+    await this.noe.approveVeterinary(vetApprouve, { from: owner });
   });
+  it('mints NFT to user by calling animalToken()', async function () {
+    await this.noe.animalToken(user1, animal1, { from: owner });
+    await this.noe.animalToken(user2, animal2, { from: owner });
+    await this.noe.animalToken(user1, animal3, { from: owner });
+    await this.noe.animalToken(user1, animal4, { from: owner });
+    expect(await this.noe.balanceOf(user1), 'user1 wrong balance').to.be.a.bignumber.equal(new BN(3));
+    expect(await this.noe.balanceOf(user2), 'user2 wrong balance').to.be.a.bignumber.equal(new BN(1));
+    const balanceOfUser1 = await this.noe.balanceOf(user1);
+    const ids = [];
+    for (let i = 0; i < balanceOfUser1; ++i) {
+      ids.push(await this.noe.tokenOfOwnerByIndex(user1, i));
+    }
+    for (let i = 0; i < balanceOfUser1; ++i) {
+      expect(await this.noe.ownerOf(ids[i])).to.equal(user1);
+    }
+    expect(await this.noe.ownerOf(2)).to.equal(user2);
+  });
+  /* describe('mint() and ownerOf()', () => {
+    it('verifies ownership after mint', async function () {
+      await this.contract.mint('3');
+      assert.equal(await this.noe.ownerOf.call(3), user1);
+    });
+  }); */
 });
